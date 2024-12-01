0c7aa0030256a245777c49ed7ef35a9d145d2548
Jannis Groth <jannisgroth@gmail.com>
2024-12-01 12:40:37 +0100

    searchbar added

--- src/app/shared/components/layout/navbar/navbar.component.ts
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterState,
+} from '@angular/router';
+import {
+  CommonModule,
+  NgComponentOutlet,
+  NgTemplateOutlet,
+} from '@angular/common';
 import { DrawerComponent } from '../../ui/drawer/drawer.component';
+import { SuchleisteComponent } from '../../ui/suchleiste/suchleiste.component';
 
 @Component({
   standalone: true,
   selector: 'app-navbar',
-  imports: [CommonModule, DrawerComponent, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    DrawerComponent,
+    RouterLink,
+    RouterLinkActive,
+    SuchleisteComponent,
+    NgTemplateOutlet,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  showSearchBar = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.showSearchBar = this.router.url === '/bibliothek';
+    });
+  }
+}
