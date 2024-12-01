a96eb9d0d103816f5b7ee28cabeee6d9308c8b61
Jannis Groth <jannisgroth@gmail.com>
2024-12-01 12:40:37 +0100

    searchbar added

---
+++ src/app/shared/components/ui/suchleiste/suchleiste.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suchleiste',
+  imports: [],
+  templateUrl: './suchleiste.component.html',
+  styleUrl: './suchleiste.component.css'
+})
+export class SuchleisteComponent {
+
+}
