686a8ef1d75fd8f877e9dccb5cb9059d3e421f41
Eremenko-Maxim <maxim@eremenko.de>
2024-12-14 13:34:17 +0100

    fix: deleted frontend-filter component

--- src/app/shared/components/ui/filtern/filtern.component.ts
+++ src/app/shared/components/ui/filtern/filtern.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { ArtFilterCollapseComponent } from './art-filter-collapse/art-filter-collapse.component';
 import { LieferbarFilterCollapseComponent } from './lieferbar-filter-collapse/lieferbar-filter-collapse.component';
 import { SchlagwoerterFilterCollapseComponent } from './schlagwoerter-filter-collapse/schlagwoerter-filter-collapse.component';
 import { Buch } from '../../../models/buch.model';
-import { FrontendFilterComponent } from './frontend-filter/frontend-filter.component';
+import { RatingFilterCollapseComponent } from './frontend-filter/rating-filter-collapse/rating-filter-collapse.component';
 
 @Component({
   selector: 'app-filtern',
@@ -11,11 +17,66 @@ import { FrontendFilterComponent } from './frontend-filter/frontend-filter.compo
     ArtFilterCollapseComponent,
     LieferbarFilterCollapseComponent,
     SchlagwoerterFilterCollapseComponent,
-    FrontendFilterComponent,
+    RatingFilterCollapseComponent,
   ],
   templateUrl: './filtern.component.html',
   styleUrl: './filtern.component.css',
 })
 export class FilternComponent {
   @Input() buecher!: WritableSignal<Buch[]>;
+  private entfernteBuecher: Buch[];
+  readonly ratingFilter = signal<number | undefined>(undefined);
+  readonly preisUntergrenzeFilter = signal<string | undefined>(undefined);
+  readonly preisObergrenzeFilter = signal<string | undefined>(undefined);
+  readonly rabattFilter = signal<string | undefined>(undefined);
+  readonly datumUntergrenzeFilter = signal<string | undefined>(undefined);
+  readonly datumObergrenzeFilter = signal<Date | undefined>(undefined);
+
+  constructor() {
+    this.entfernteBuecher = [];
+  }
+
+  /**
+   * Filtert eine Buchliste anhand der Kriterien, die sich nicht über eine
+   * GET Request filtern lassen.
+   */
+  frontendFilter() {
+    const alleBuecher = [...this.buecher(), ...this.entfernteBuecher];
+    let passendeElemente: Buch[] = [];
+    let herausgefilterteElemente: Buch[] = [];
+
+    alleBuecher.forEach(buch => {
+      // Rating Filter ist gesetzt und kleiner gleich Buchrating
+      this.ratingFilter() && this.ratingFilter()! <= buch.rating!
+        ? passendeElemente.push(buch)
+        : herausgefilterteElemente.push(buch);
+
+      // Filter für Preisober- und Untergrenze sind gesetzt und
+      // Buchpreis ist dazwischen
+      // Typecasting von string zu number mit Unary Plus Operator (+)
+      this.preisUntergrenzeFilter() &&
+      this.preisObergrenzeFilter() &&
+      +this.preisUntergrenzeFilter()! <= +buch.preis &&
+      +buch.preis <= +this.preisObergrenzeFilter()!
+        ? passendeElemente.push(buch)
+        : herausgefilterteElemente.push(buch);
+
+      // Filter für Rabatt ist gesetzt und kleiner gleich Buchrabatt
+      this.rabattFilter() && this.rabattFilter()! <= buch.rabatt!
+        ? passendeElemente.push(buch)
+        : herausgefilterteElemente.push(buch);
+
+      // Filter für Datumsober- und Untergrenze sind gesetzt und
+      // Buchdatum ist dazwischen
+      this.datumUntergrenzeFilter() &&
+      this.datumObergrenzeFilter() &&
+      this.datumUntergrenzeFilter()! <= buch.datum! &&
+      buch.datum! <= this.datumObergrenzeFilter()!
+        ? passendeElemente.push(buch)
+        : herausgefilterteElemente.push(buch);
+    });
+
+    this.buecher.set(passendeElemente);
+    this.entfernteBuecher = herausgefilterteElemente;
+  }
 }
--- src/app/shared/components/ui/filtern/frontend-filter/rating-filter-collapse/rating-filter-collapse.component.ts
+++ src/app/shared/components/ui/filtern/frontend-filter/rating-filter-collapse/rating-filter-collapse.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, WritableSignal } from '@angular/core';
+import { FilternComponent } from '../../filtern.component';
 
 @Component({
   selector: 'app-rating-filter-collapse',
   imports: [],
   templateUrl: './rating-filter-collapse.component.html',
-  styleUrl: './rating-filter-collapse.component.css'
+  styleUrl: './rating-filter-collapse.component.css',
 })
 export class RatingFilterCollapseComponent {
+  @Input() ratingFilter!: WritableSignal<number | undefined>;
+  readonly filter: FilternComponent;
 
+  constructor() {
+    this.filter = new FilternComponent();
+  }
+
+  RatingFilterSetter(rating: number) {
+    this.ratingFilter.set(rating);
+    this.filter.frontendFilter();
+  }
+
+  uncheck(target: EventTarget) {
+    console.log((target as HTMLInputElement).checked);
+    if (!(target as HTMLInputElement).checked) {
+      const defaultRating = document.querySelector<HTMLInputElement>(
+        '[name="defaultRatingFilter"]'
+      );
+      (defaultRating as HTMLInputElement).checked = true;
+      this.ratingFilter.set(undefined);
+      setTimeout(() => {
+        this.filter.frontendFilter();
+      }, 200);
+    }
+  }
 }
