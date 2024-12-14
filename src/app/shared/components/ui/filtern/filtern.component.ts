f8e018a4f0afcc82dbed329577532b90512ff92f
Eremenko-Maxim <maxim@eremenko.de>
2024-12-14 10:45:35 +0100

    feat: declared signal variables for frontend filtering

--- src/app/shared/components/ui/filtern/filtern.component.ts
+++ src/app/shared/components/ui/filtern/filtern.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, WritableSignal } from '@angular/core';
+import { Component, Input, signal, WritableSignal } from '@angular/core';
 import { ArtFilterCollapseComponent } from './art-filter-collapse/art-filter-collapse.component';
 import { LieferbarFilterCollapseComponent } from './lieferbar-filter-collapse/lieferbar-filter-collapse.component';
 import { SchlagwoerterFilterCollapseComponent } from './schlagwoerter-filter-collapse/schlagwoerter-filter-collapse.component';
+import { Buch } from '../../../models/buch.model';
+import { FrontendFilterComponent } from './frontend-filter/frontend-filter.component';
 
 @Component({
   selector: 'app-filtern',
@@ -9,8 +11,11 @@ import { SchlagwoerterFilterCollapseComponent } from './schlagwoerter-filter-col
     ArtFilterCollapseComponent,
     LieferbarFilterCollapseComponent,
     SchlagwoerterFilterCollapseComponent,
+    FrontendFilterComponent,
   ],
   templateUrl: './filtern.component.html',
   styleUrl: './filtern.component.css',
 })
-export class FilternComponent {}
+export class FilternComponent {
+  @Input() buecher!: WritableSignal<Buch[]>;
+}
--- src/app/shared/components/ui/filtern/frontend-filter/frontend-filter.component.ts
+++ src/app/shared/components/ui/filtern/frontend-filter/frontend-filter.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal, WritableSignal } from '@angular/core';
+import { Buch } from '../../../../models/buch.model';
 
 @Component({
   selector: 'app-frontend-filter',
   imports: [],
   templateUrl: './frontend-filter.component.html',
-  styleUrl: './frontend-filter.component.css'
+  styleUrl: './frontend-filter.component.css',
 })
 export class FrontendFilterComponent {
+  @Input() buecher!: WritableSignal<Buch[]>;
+  gefilterteBuecher: Buch[];
+  readonly ratingFilter = signal<number | undefined>(undefined);
+  readonly preisUntergrenzeFilter = signal<string | undefined>(undefined);
+  readonly preisObergrenzeFilter = signal<string | undefined>(undefined);
+  readonly rabattFilter = signal<string | undefined>(undefined);
+  readonly datumUntergrenzeFilter = signal<string | undefined>(undefined);
+  readonly datumObergrenzeFilter = signal<Date | undefined>(undefined);
 
+  constructor() {
+    this.gefilterteBuecher = [];
+  }
+
+  frontendFilter() {}
 }
