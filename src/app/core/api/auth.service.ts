2c3b332f33a4873187155cacbb47d8a8b2dcc6ab
Raphael Theiss <theissraphael82@gmail.com>
2024-12-18 21:13:58 +0100

    Login Form validierung, Token speicherung in signal

--- src/app/core/api/auth.service.ts
+++ src/app/core/api/auth.service.ts
@@ -1,26 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private token!: string;
+  protected token = signal('');
   private readonly tokenUrl = 'https://localhost:3000/auth/token';
 
   constructor(private http: HttpClient) {}
 
-  async getToken() {
-    const body = {
-      username: 'admin',
-      password: 'p',
-    };
-    this.http.post(this.tokenUrl, body).subscribe({
+  async getToken(loginDaten: { username: string; password: string }) {
+    console.log(loginDaten);
+    this.http.post<{ access_token: string; [key: string]: any }>(this.tokenUrl, loginDaten).subscribe({
       next: response => {
         console.log(response);
+        this.token.set(response.access_token);
+        console.log(this.token());
       },
       error: error => {
         console.log(error);
+        alert('Login Fehlgeschlagen!');
       },
     });
   }
--- src/app/shared/components/ui/login/login.component.ts
+++ src/app/shared/components/ui/login/login.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../../../core/api/auth.service';
 import { getTestBed } from '@angular/core/testing';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Token } from '@angular/compiler';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private auth: AuthService) {}
+  loginForm: FormGroup = new FormGroup({
+    benutzername: new FormControl(undefined, Validators.required),
+    passwort: new FormControl(undefined, Validators.required),
+  });
+
+  constructor(private auth: AuthService) { }
 
   async onLogin() {
-    await this.auth.getToken();
+    if (this.loginForm.valid) {
+      await this.auth.getToken({
+        username: this.loginForm.get('benutzername')!.value,
+        password: this.loginForm.get('passwort')!.value
+      });
+    }
   }
 }
