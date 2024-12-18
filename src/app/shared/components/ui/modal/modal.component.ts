a8ba7c266ac60c29722d05ad9c3a3d09969335b8
luluiisss <168561911+luluiisss@users.noreply.github.com>
2024-12-18 21:14:34 +0100

    Rabatt- und Preis-Rechnung

--- src/app/shared/components/ui/modal/modal.component.ts
+++ src/app/shared/components/ui/modal/modal.component.ts
@@ -19,4 +19,22 @@ export class ModalComponent {
   closeModal(modal: HTMLDialogElement) {
     modal.close();
   }
+
+  get berechneterRabatt(): number | undefined {
+    if (!this.buch) return undefined;
+    if (this.buch.rabatt == undefined || null) return undefined;
+
+    const rabatt = Number(this.buch.rabatt) * 100; // Rabatt-string in eine Zahl konvertieren
+    return Math.round(rabatt * 100) / 100; // Runden auf 2 Dezimalstellen
+  }
+
+  get berechneterPreis(): number | undefined {
+    if (!this.buch) return undefined;
+
+    const preis = Number(this.buch.preis); // Preis-string in eine Zahl konvertieren
+    const rabatt = this.buch.rabatt ? Number(this.buch.rabatt) : 0; // Rabatt direkt aus der Buch-Entität holen
+
+    const berechneterPreis = preis * (1 - rabatt); // Berechnung des Preises mit Rabatt
+    return Math.round(berechneterPreis * 100) / 100; // Runden auf 2 Dezimalstellen
+  }
 }
