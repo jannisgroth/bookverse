3ab62f7af2d52c3c9d4babe9b31a7e33e87bdc02
Jannis Groth <jannisgroth@gmail.com>
2024-12-18 18:57:39 +0100

    overview changed

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,42 +1,12 @@
 import { Component, Injectable, OnInit, signal } from '@angular/core';
-import { CarouselComponent } from '../../shared/components/ui/carousel/carousel.component';
 import { ReadService } from '../../core/api/http-read.service';
-import { ErrorAlertComponent } from '../../shared/components/ui/alerts/error-alert/error-alert.component';
-import { NgIf } from '@angular/common';
-import { LoggerService } from '../../core/logging/logger.service';
+import { LoginComponent } from '../../shared/components/ui/login/login.component';
 
 @Component({
   selector: 'app-overview',
-  imports: [CarouselComponent, ErrorAlertComponent, NgIf],
+  imports: [LoginComponent],
   providers: [ReadService],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
 })
-export class OverviewComponent implements OnInit {
-  constructor(
-    private readservice: ReadService,
-    private logger: LoggerService
-  ) {}
-  get buecher() {
-    return this.readservice.buecher;
-  }
-  get loading() {
-    return this.readservice.loading;
-  }
-  get errorBilderShow() {
-    return this.readservice.errorBilder().show;
-  }
-  get errorBilderMessage() {
-    return this.readservice.errorBilder().message;
-  }
-  get errorShow() {
-    return this.readservice.error().show;
-  }
-  get errorMessage() {
-    return this.readservice.error().message;
-  }
-
-  ngOnInit() {
-    this.readservice.getBuecherMitBild();
-  }
-}
+export class OverviewComponent {}
