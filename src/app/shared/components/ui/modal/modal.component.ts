d91d6caa7cd8ab6a9a1bd99508255f1badc29cbb
luluiisss <168561911+luluiisss@users.noreply.github.com>
2025-01-09 16:53:36 +0100

    bug fix

--- src/app/shared/components/ui/modal/modal.component.ts
+++ src/app/shared/components/ui/modal/modal.component.ts
@@ -25,17 +25,17 @@ export class ModalComponent {
 
   get berechneterRabatt() {
     if (!this.buch) return undefined;
-    if (this.buch()!.rabatt === undefined) return undefined;
+    if (this.buch()?.rabatt === undefined) return undefined;
 
-    const rabatt = Number(this.buch()!.rabatt) * 100; // Rabatt-string in eine Zahl konvertieren
+    const rabatt = Number(this.buch()?.rabatt) * 100; // Rabatt-string in eine Zahl konvertieren
     return Math.round(rabatt * 100) / 100; // Runden auf 2 Dezimalstellen
   }
 
   get berechneterPreis() {
     if (!this.buch) return undefined;
 
-    const preis = Number(this.buch()!.preis); // Preis-string in eine Zahl konvertieren
-    const rabatt = this.buch()!.rabatt ? Number(this.buch()!.rabatt) : 0; // Rabatt direkt aus der Buch-Entität holen
+    const preis = Number(this.buch()?.preis); // Preis-string in eine Zahl konvertieren
+    const rabatt = this.buch()?.rabatt ? Number(this.buch()!.rabatt) : 0; // Rabatt direkt aus der Buch-Entität holen
 
     const berechneterPreis = preis * (1 - rabatt); // Berechnung des Preises mit Rabatt
     return Math.round(berechneterPreis * 100) / 100; // Runden auf 2 Dezimalstellen
